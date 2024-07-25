30096e00dd52ceb5318168dfebb8be993fd7399a
CamiloCastrillon <juan.navia.castrillon@correounivalle.edu.co>
2024-07-24 19:11:23 -0500

    creando optuna study

--- resources/opti_full_auto.py
+++ resources/opti_full_auto.py
@@ -1,137 +1,99 @@
 import os
 import optuna
 import numpy as np
-from resources.message import error_message, warning_message, method_menssage
+from resources.message import method_menssage
 from typing import Union
+from resources.create_architecture import CreateFullAuto as cfa
 
 class OptimizeFullAuto:
     """
     Crea el objeto de estudio con optuna para optimizar el full auto encoder y permite consultar los parámetros obtenidos
     """
-    def __init__(self, dataset:np.ndarray, nl_min:int, nl_max:int, dim:int, lr_min:float, lr_max:float, ep_min:int, ep_max:int, ba_min:int, ba_max:int, ink:int, mode_l1:Union[str, None], rl1_min:float, rl1_max:float, mode_l2:Union[str, None], rl2_min:float, rl2_max:float, mode_do:Union[str, None], do_min:float, do_max:float, esp_min:int, esp_max:int) -> None:
+    def __init__(self, dataset:np.ndarray=None, nl_min:int=None, nl_max:int=None, dim:int=None, lr_min:float=None, lr_max:float=None, ep_min:int=None, ep_max:int=None, ba_min:int=None, ba_max:int=None, ink:int=None, mode_l1:Union[str, None]=None, rl1_min:float=None, rl1_max:float=None, mode_l2:Union[str, None]=None, rl2_min:float=None, rl2_max:float=None, mode_do:Union[str, None]=None, do_min:float=None, do_max:float=None, esp_min:int=None, esp_max:int=None) -> None:
         """
         Inicializa los argumentos de la clase.
         
         Args:
-            dataset (np.ndarray):
-            nl_min
+            dataset (np.ndarray): Dataset.Conjunto de datos de entrenamiento.
+            nl_min  (int): Minimum number of layers. Número mínimo de capas en el encoder.
+            nl_max  (int): Maximum number of layers. Número máximo de capas en el encoder.
+            dim     (int): Dimension. Valor la dimensión m de los datos de entrada (mxmx3).
+            lr_min  (float): Minimum Learning Rate. Taza de aprendizaje mínima.
+            lr_max  (float): Maximum Learning Rate. Taza de aprendizaje máxima.
+            ep_min  (int): Minimum number of epochs. Número de épocas de mínimas de entrenamiento.
+            ep_max  (int): Maximum number of epochs. Número de épocas de máximas de entrenamiento.
+            ba_min  (int): Minimum Batch Size. Tamaño de lote mínimo.
+            ba_max  (int): Maximum Batch Size. Tamaño de lote máximo.
+            ink     (int): Initial number of kernels: Número de kernels para iniciar el full autoencoder
+            mode_l1 (Union[str,None]): Modo de uso de regularización l1.
+                - 'all':    Todas las capas tendrán regularización l1.
+                - 'random': Capas elegidas aleatoriamente tendrán regularización l1.
+                - None:     Ninguna capa tendrá regularización l1.
+            rl1_min (float): Minimum L1 regularization. Regularización L1 mínima.
+            rl1_max (float): Maximum L1 regularization. Regularización L1 máxima.
+            mode_l2 (Union[str,None]): Modo de uso de regularización l2.
+                - 'all':    Todas las capas tendrán regularización l2.
+                - 'random': Capas elegidas aleatoriamente tendrán regularización l2.
+                - None:     Ninguna capa tendrá regularización l2.
+            rl2_min (float): Minimum L2 regularization. Regularización L2 mínima.
+            rl2_max (float): Maximum L2 regularization. Regularización L2 máxima.
+            mode_do (Union[str,None]): Modo de uso de drop out.
+                - 'all':    Todas las capas tendrán drop out.
+                - 'random': Capas elegidas aleatoriamente tendrán drop out.
+                - None:     Ninguna capa tendrá drop out.
+            do_min  (float): Minumum DropOut. Porcentaje de apagado de neuronas mínimo.
+            do_max  (float): Maximum DropOut. Porcentaje de apagado de neuronas máximo.
+            esp_min (int): Minimum Early Stopping Patience. Número mínimo de epocas de espera para la para temprana.
+            esp_max (int): Maximum Early Stopping Patience. Número máximo de epocas de espera para la para temprana.
         """
-        self.dataset    = dataset   # Dataset: Conjunto de datos de entrenamiento
-        self.nl_min     = nl_min    # Minimum number of layers: Número mínimo de capas en el encoder
-        self.nl_max     = nl_max    # Maximum number of layers: Número máximo de capas en el encoder
-        self.dim        = dim       # Dimension: Valor la dimensión m de los datos de entrada (mxmx3)
-        self.lr_min     = lr_min    # Minimum Learning Rate: Taza de aprendizaje mínima
-        self.lr_max     = lr_max    # Maximum Learning Rate: Taza de aprendizaje máxima
-        self.ep_min     = ep_min    # Minimum number of epochs: Número de épocas de mínimas de entrenamiento
-        self.ep_max     = ep_max    # Maximum number of epochs: Número de épocas de máximas de entrenamiento
-        self.ba_min     = ba_min    # Minimum Batch Size: Tamaño de lote mínimo
-        self.ba_max     = ba_max    # Maximum Batch Size: Tamaño de lote máximo
-        self.ink        = ink       # Initial number of kernels: Número de kernels para iniciar el full autoencoder
+        self.dataset    = dataset
+        self.nl_min     = nl_min
+        self.nl_max     = nl_max
+        self.dim        = dim
+        self.lr_min     = lr_min
+        self.lr_max     = lr_max
+        self.ep_min     = ep_min
+        self.ep_max     = ep_max 
+        self.ba_min     = ba_min
+        self.ba_max     = ba_max 
+        self.ink        = ink
         self.mode_l1    = mode_l1
-        self.rl1_min    = rl1_min   # Minimum L1 regularization: Regularización L1 mínima
-        self.rl1_max    = rl1_max   # Maximum L1 regularization: Regularización L1 máxima
+        self.rl1_min    = rl1_min
+        self.rl1_max    = rl1_max
         self.mode_l2    = mode_l2
-        self.rl2_min    = rl2_min   # Minimum L2 regularization: Regularización L2 mínima
-        self.rl2_max    = rl2_max   # Maximum L2 regularization: Regularización L2 máxima
+        self.rl2_min    = rl2_min
+        self.rl2_max    = rl2_max
         self.mode_do    = mode_do
-        self.do_min     = do_min    # Minumum DropOut: Porcentaje de apagado de neuronas mínimo
-        self.do_max     = do_max    # Maximum DropOut:Porcentaje de apagado de neuronas máximo
-        self.esp_min    = esp_min   # Minimum Early Stopping Patience: Número mínimo de epocas de espera para la para temprana
-        self.esp_max    = esp_max   # Maximum Early Stopping Patience: Número máximo de epocas de espera para la para temprana
-
-    def check_type(self, var, type, label):
-        if not isinstance(var, type):
-            error_message(f'la variable "{label}" debe ser de tipo {type.__name__}.')
-
-    def check_numeric_min_max(self, var_min, var_max, var_type, label_min, label_max):
-        self.check_type(var_min, var_type, label_min)
-        self.check_type(var_max, var_type, label_max)
-        if var_min >= var_max:
-            error_message(f'La variable "{label_min}" debe ser menor al número máximo.')
-        elif var_min <= 0:
-            error_message(f'La variable "{label_min}" debe ser mayor a cero.')
-
-    def check_limits(self, var, l_min, l_max, label):
-        if not l_min <= var <= l_max:
-            warning_message(f'Se recomienda que la variable "{label}" tenga valores entre {l_min} y {l_max}. No es obligatorio para la ejecución del algoritmo, pero puede afectar en los resultados del entrenamiento.')
-
-    def check_values(self):
-        """
-        Evalua los posibles errores al ingresar los argumentos de la clase
-        """
-        method_menssage(self.check_values.__name__, 'Verifica los posibles errores y advertencias al ingresar los argumentos de la clase OptimizeFullAuto')
-        # Evalua el tipo de la variable dataset
-        self.check_type(self.dataset, np.ndarray, 'dataset')
-        if not self.dataset.ndim == 5:
-            error_message('Las dimensiones de la variable dataset no son las cinco esperadas.')
-
-        # Evalua las variables nl_min y nl_max
-        self.check_numeric_min_max(self.nl_min, self.nl_max, int, 'número de capas mínimo', 'número de capas máximo')
-
-        # Evalua la variable dim
-        self.check_type(self.dim, int, 'dimensión de los datos de entrada')
-
-        # Evalua la dimensión de los datos de entrada y si corresponde al número de capas máximo
-        cheack_dim = self.dim
-        for i in range(self.nl_max):
-            cheack_dim /= 2
-            if round(cheack_dim) < 3:         
-                error_message('El encoder tiene una cantidad máxima de capas superior a la esperada por la dimensión de los datos de entrenamiento.')
-
-        # Evalua la variable lr_min y lr_max
-        self.check_numeric_min_max(self.lr_min, self.lr_max, float, 'learning rate mínimo', 'learning rate máximo')
-
-        # Evalua si los valores de lr_min y lr_max son recomendados
-        self.check_limits(self.lr_min, 0.0001, 0.001, 'learning rate mínimo')
-        self.check_limits(self.lr_max, 0.01, 0.1, 'learning rate máximo')
-        
-        # Evalua la variable ep_min y ep_max
-        self.check_numeric_min_max(self.ep_min, self.ep_max, int, 'número de epocas de entrenamiento mínimas', 'número de epocas de entrenamiento mínimas')
-
-        # Evalua la variable ba_min y ba_max
-        self.check_numeric_min_max(self.ba_min, self.ba_max, int, 'batch size mínimo', 'batch size máximo')
-
-        # Evalua que el ba_max no sea mayor o igual al número de datos por muestra del dataset
-        if self.ba_max >= self.dataset.shape[1]:
-            error_message('El batch size máximo no puede ser mayor al número de datos por muestra del dataset.')
-        
-        # Evalua la variable ink
-        self.check_type(self.ink, int, 'número inicial de kernels')
-        if self.ink <= 0:
-            error_message('La variable del número inicial de kernels debe ser un número mayor a cero.')
-        elif not self.ink % 2 == 0:
-            error_message('La variable del número incial de kernels debe ser un número par.')
-        self.check_limits(self.ink, 8, 32, 'número inicial de kernels')
-
-        # Evalua la variable rl1_min y rl1_max
-        self.check_numeric_min_max(self.rl1_min, self.rl1_max, float, 'regularización l1 mínima', 'regularización l1 máxima')
-        self.check_limits(self.rl1_min, 0.00001, 0.0001, 'regularización l1 mínima')
-        self.check_limits(self.rl1_max, 0.001, 0.1, 'regularización l1 máxima')
-
-        # Evalua la variable rl2_min y rl2_max
-        self.check_numeric_min_max(self.rl2_min, self.rl2_max, float, 'regularización l2 mínima', 'regularización l2 máxima')
-        self.check_limits(self.rl2_min, 0.00001, 0.0001, 'regularización l2 mínima')
-        self.check_limits(self.rl2_max, 0.001, 0.1, 'regularización l2 máxima')
-
-        # Evalua las variables do_min y do_max
-        self.check_numeric_min_max(self.do_min, self.do_max, float, 'drop out mínimo', 'drop out máximo')
-        self.check_limits(self.do_min, 0.1, 0.2, 'drop out mínimo')
-        self.check_limits(self.do_max, 0.2, 0.4, 'drop out máximo')
-
-        # Evalua las variables esp_min y esp_max
-        self.check_numeric_min_max(self.esp_min, self.esp_max, int, 'número mínimo de epocas de espera para la para temprana', 'número máximo de epocas de espera para la para temprana')
-        self.check_limits(self.esp_min, 1, 10, 'número mínimo de epocas de espera para la para temprana')
-        self.check_limits(self.esp_max, 10, 20, 'número máximo de epocas de espera para la para temprana')
+        self.do_min     = do_min
+        self.do_max     = do_max
+        self.esp_min    = esp_min
+        self.esp_max    = esp_max
 
     def optuna_study(self, trial):
         """
-        Crea el objeto de estudio de optuna
+        Crea el objeto de estudio de optuna.
         """
-        method_menssage(self.optuna_study.__name__)
-        self.check_values()
-        
-        number_layers   = trial.suggest_int('number_layers', self.nl_min, self.nl_max)
-        learning_rate   = trial.suggest_float('learning_rate', self.nl_min, self.nl_max)
-        epochs          = trial.suggest_int('epochs', self.ep_min, self.ep_max) 
-        batch_size      = trial.suggest_int('batch_size', self.ba_min, self.ba_max)
-        kernels         = self.ink
+        method_menssage(self.optuna_study.__name__, 'Crea el objeto de estudio de optuna')
+        # Parámetros a variar de la arquitectura.
+        nl  = trial.suggest_int('number_layers', self.nl_min, self.nl_max)
+        l1  = trial.suggest_float('l1_regularization', self.rl1_min, self.rl1_max)
+        l2  = trial.suggest_float('l2_regularization', self.rl2_min, self.rl2_max)
+        do  = trial.suggest_float('drop_out', self.do_min, self.do_max)
+        lr  = trial.suggest_float('learning_rate', self.nl_min, self.nl_max)
+        # Parámetros a variar en el entrenamiento.
+        esp = trial.suggest_int('early_stopping', self.esp_min, self.esp_max)
+        ep  = trial.suggest_int('epochs', self.ep_min, self.ep_max) 
+        bs  = trial.suggest_int('batch_size', self.ba_min, self.ba_max)
+        # Crea la arquitectura
+        model = cfa().create_model('y', 'y', self.ink, self.dim, nl, self.mode_l1, self.mode_l2, l1, l2, self.mode_do, do, lr)
+        # Entrena la arquitectuira
+        _, history00 = cfa().train_model('y', self.dataset, esp, ep, bs, self.dim)
+        # Obtiene la pérdida del entrenamiento
+        val_loss = history00.history['val_loss'][-1]
+        return val_loss
+    
+    def execute_study(self):
+        study = optuna.create_study(direction='minimize')   # Crea el estudio con el objetivo de minimizar la pérdida del modelo.
+        study.optimize(self.optuna_study, n_trials=5)       # Ejecuta el estudio.
+
+        return print(f'\nMejores parametros encontrados: {study.best_params}\nPéridida más baja en la configuración optima: {study.best_value}')
