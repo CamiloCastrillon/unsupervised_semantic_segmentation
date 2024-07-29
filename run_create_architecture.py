2a6f0ca406fd60a0e7b466962cfaca6bd1cff8fb
CamiloCastrillon <juan.navia.castrillon@correounivalle.edu.co>
2024-07-29 12:12:18 -0500

    .
    .

--- run_create_architecture.py
+++ run_create_architecture.py
@@ -12,7 +12,7 @@ param_l2=None
 mode_do=None
 param_do=None
 lr=0.001
-pth_save_model = 'C:/camilo/resources_uss/modelos/full_auto_encoder/'
+pth_save_model = 'C:/camilo/resources_uss/models/full_auto_encoder/'
 
 model_fa = cfa().create_model(verify_errors, verify_warnings, kernels, dim, number_layers, mode_l1, mode_l2, param_l1, param_l2, mode_do, param_do, lr)
 print(f'\n{model_fa.summary()}\n')
