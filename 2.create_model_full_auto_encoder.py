a12710e7542399df7cdd448b06f6944233108bfe
CamiloCastrillon <103201304+CamiloCastrillon@users.noreply.github.com>
2024-09-29 15:46:14 -0500

    .
    .

--- 2.create_model_full_auto_encoder.py
+++ 2.create_model_full_auto_encoder.py
@@ -16,3 +16,4 @@ pth_save_model = 'C:/camilo/uss/models/full_auto_encoder/'
 model_fa = cfa().create_model(verify_errors, verify_warnings, kernels, dim, number_layers, mode_l1, mode_l2, param_l1, param_l2, mode_do, param_do)
 print(f'\n{model_fa.summary()}\n')
 #cfa().save_model('y', model_fa, 'full_auto_encoder', pth_save_model)
+
