156ee5104d95e1e8a014c0440e38f6b64824ce48
CamiloCastrillon <103201304+CamiloCastrillon@users.noreply.github.com>
2024-11-11 12:48:53 -0500

    .
    .

--- kmeans/deploy_kmeans.py
+++ kmeans/deploy_kmeans.py
@@ -2,14 +2,12 @@ import cv2
 import os
 import numpy as np
 import pickle
-from PIL import Image
 
+folder_save = 'C:/camilo/trabajo_de_grado/imgs/aguacate_kmeans/'
+path_model  = 'C:/camilo/trabajo_de_grado/imgs/models/kmeans_model.pkl'
+folder_imgs = 'C:/camilo/trabajo_de_grado/imgs/dataset/'
 
-folder_save = 'C:/camilo/imgs/aguacate_kmeans/'
-path_model  = 'C:/camilo/imgs/models/kmeans_model.pkl'
-folder_imgs         = 'C:/camilo/imgs/aguacate/'
-
-files_imgs          =  os.listdir(folder_imgs)
+files_imgs  =  os.listdir(folder_imgs)
 
 # Cargar el modelo entrenado
 print('Cargando modelo.')
@@ -21,14 +19,38 @@ print('Implementando modelo.\n')
 for name_img in files_imgs:
     entire_path = folder_imgs+name_img
     img         = cv2.imread(entire_path)
-    w, h, _     = img.shape
+    print(img.shape)
+    h, w, _     = img.shape
     img_flatten = img.reshape(-1, 3)
 
     # Obtener las etiquetas de los clusters y los centros de los clusters
     labels = kmeans.predict(img_flatten)
-    # Crea la imagen rgba
-    img_class = labels.reshape(w, h).astype(np.uint8)
-    #img_class = np.dstack((img, labels.reshape(w, h).astype(np.uint8)))
-    path_save = folder_save + name_img[:-4] + '_kmeans.tiff'
-    cv2.imwrite(path_save, img_class)
-    print(f'    ->Imagen {name_img[:-4]}_kmeans.tiff guardada.')
+
+    # Reshape de las etiquetas para que coincidan con la forma original de la imagen (w, h)
+    labels_reshaped = labels.reshape(h, w)
+
+    # Crear una nueva matriz con la imagen RGB y las etiquetas como la cuarta banda
+    img_with_labels = np.dstack((img, labels_reshaped))
+
+    # Guardar la imagen con la cuarta banda en formato npy
+    save_path = os.path.join(folder_save, f'{name_img[:-4]}_data.npy')
+    np.save(save_path, img_with_labels)
+    
+    """
+    # Crear un array para guardar los datos RGB y la clase
+    data = np.empty((h, w, 4), dtype=np.uint8)  # 4 canales: R, G, B, class
+    
+    # Rellenar el array con los valores RGB y la clase
+    for i in range(len(labels)):
+        y = i // w
+        x = i % w
+        data[y, x, 0] = img_flatten[i, 0]  # R
+        data[y, x, 1] = img_flatten[i, 1]  # G
+        data[y, x, 2] = img_flatten[i, 2]  # B
+        data[y, x, 3] = labels[i]           # Clase
+
+    # Guardar la imagen como archivo NumPy
+    path_npy = folder_save + name_img[:-4] + '_data.npy'
+    np.save(path_npy, data)
+    """
+    print(f'    ->Datos de {name_img[:-4]} guardados en {save_path}.')
